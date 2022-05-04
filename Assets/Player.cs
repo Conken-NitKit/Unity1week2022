b7f3b0e7b585910641f81c4c2912de429d712e23
Teshima <takuto0418340@gmail.com>
2022-05-04 17:35:51 +0900

    ＃２修正：MoveTimeをMoveSecondsに変更しました

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -15,7 +15,7 @@ public class Player : MonoBehaviour
     private const float UpYPosition = 2.5f;
     private const float DownYPosition = -2.5f;
 
-    private const float MoveTime = 0.01f;
+    private const float MoveSeconds = 0.01f;
 
     private bool pushOtherKey = false;
     private bool pushRightArrowKey = false;
@@ -35,35 +35,35 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.RightArrow) && pushOtherKey == false)
         {
-            this.transform.DOMove(new Vector2(RightXPositon,DefaultYPosition),MoveTime);
+            this.transform.DOMove(new Vector2(RightXPositon,DefaultYPosition),MoveSeconds);
             pushOtherKey = true;
             pushRightArrowKey = true;
         }
 
         if(Input.GetKey(KeyCode.LeftArrow) && pushOtherKey == false)
         {
-            this.transform.DOMove(new Vector2(LeftXPosition,DefaultYPosition),MoveTime);
+            this.transform.DOMove(new Vector2(LeftXPosition,DefaultYPosition),MoveSeconds);
             pushOtherKey = true;
             pushLeftArrowKey = true;
         }
 
         if(Input.GetKey(KeyCode.UpArrow) && pushOtherKey == false)
         {
-            this.transform.DOMove(new Vector2(DefaultXPosition,UpYPosition),MoveTime);
+            this.transform.DOMove(new Vector2(DefaultXPosition,UpYPosition),MoveSeconds);
             pushOtherKey = true;
             pushUpArrowKey = true;
         }
 
         if(Input.GetKey(KeyCode.DownArrow) && pushOtherKey == false)
         {
-            this.transform.DOMove(new Vector2(DefaultXPosition,DownYPosition),MoveTime);
+            this.transform.DOMove(new Vector2(DefaultXPosition,DownYPosition),MoveSeconds);
             pushOtherKey = true;
             pushDownArrowKey = true;
         }
 
         if((Input.GetKeyUp(KeyCode.RightArrow) && pushRightArrowKey == true) || (Input.GetKeyUp(KeyCode.LeftArrow) && pushLeftArrowKey == true) || (Input.GetKeyUp(KeyCode.UpArrow) && pushUpArrowKey == true) || (Input.GetKeyUp(KeyCode.DownArrow) && pushDownArrowKey == true))
         {
-            this.transform.DOMove(new Vector2(DefaultXPosition,DefaultYPosition),MoveTime);
+            this.transform.DOMove(new Vector2(DefaultXPosition,DefaultYPosition),MoveSeconds);
             pushOtherKey = false;
             if(pushRightArrowKey == true)
             {
