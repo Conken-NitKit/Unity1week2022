d3514407ecd8221f2a9bb529e4d64b6b5069f23c
satwu-0 <satouajinomoti@gmail.com>
2022-05-11 16:12:18 +0900

    #24追加：ゲームオーバーの画面とタイトルに戻るボタン、そのシーン遷移を追加しました．

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+/// <summary>
+/// スコアの加算を管理するクラス
+/// </summary>
+public class ScoreText : MonoBehaviour
+{
+    [SerializeField]private WallAnimation wallAnimation;
+    public Text scoreText;
+
+    private void Start()
+    {
+       SetScore(); 
+    }
+    
+    public void SetScore()
+    {
+        scoreText.text = "回避数 " + wallAnimation.ToString() + "回" ;
+    }    
+}
